7371ab804fcee8383ea9b187e9b07b13408e2fa7
Samvel <movetonjkee@gmail.com>
2019-11-18 16:44:28 +0300

    layout section serf - slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,6 +12,12 @@ $(function(){
         slidesToScroll: 1,
         asNavFor: '.header__slider'
     });
+    $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>'
+    });
 
   
 });
